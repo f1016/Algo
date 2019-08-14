7700ff7500ef6dc311a4a18eede7a62b1b22d6c8
f1016 <47860894+f1016@users.noreply.github.com>
2019-08-14 17:38:21 +0800

    Update DP.java

--- Dynamic Programming/62. Unique Paths/DP.java
+++ Dynamic Programming/62. Unique Paths/DP.java
@@ -27,3 +27,33 @@ class Solution {
         return map[m-1][n-1];
     }
 }
+
+
+//using only 1D array
+//LOGIC: since every position is calculated by the top one and the left one
+//LOGIC: the one is used to calculated is useless, so we can just replace them
+/*
+class Solution {
+
+    public int uniquePaths(int m, int n) {
+        if( n <= 1 || m <= 1){
+            return 1;
+        }
+        
+        int map[]= new int[n];
+
+        for(int i = 0 ; i < n ; i ++){
+            map[i] = 1;
+        }
+                
+        for(int i = 1 ; i < m ; i ++){
+            for(int j = 1 ; j < n ; j ++){
+               map[j] = map[j] + map[j-1]; 
+            }
+			printf("%d'n",map[n-1]);
+        }
+        
+        return map[n-1];
+    }
+}
+*/
