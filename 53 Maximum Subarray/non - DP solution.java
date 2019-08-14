6139a7e099cbe2c62b9d5e046d0c0bfcd50ac63b
f1016 <47860894+f1016@users.noreply.github.com>
2019-08-14 14:44:58 +0800

    Add files via upload

---
+++ 53 Maximum Subarray/non - DP solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int cur = nums[0];
+        int max = nums[0];
+        
+        for(int i = 1 ; i < nums.length; i++){
+            if(cur < 0 ){
+                
+                cur = nums[i];
+                if(cur > max){
+                    max = cur;
+                }
+                
+            }
+            else{
+                cur += nums[i];
+                if(cur > max){
+                    max = cur;
+                }
+                
+            }
+        }
+        return max;
+    }
+}
